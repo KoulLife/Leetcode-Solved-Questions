b4afa8737c3ef6a38302e032b170aa4bedc7406b
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2023-06-29 23:04:15 +0900

    Time: 230 ms (86.36%), Space: 23.5 MB (85.69%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,31 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [0] * len(nums)
-        newHash = {}
-        sum = 1
-        zeroNum = 0
+        out = []
+        p = 1
         
         for i in range(0, len(nums)):
-            newHash[i] = nums[i]
-            if nums[i] == 0:
-                zeroNum += 1
-            else:
-                sum = sum * nums[i]
+            out.append(p)
+            p = p * nums[i]
         
-        if zeroNum == 1:
-            for k in range(0, len(nums)):
-                if nums[k] == 0:
-                    res[k] = sum
-                else:
-                    res[k] = 0
+        p = 1
         
-        elif zeroNum > 1:
-            for l in range(0, len(nums)):
-                res[l] = 0
+        for i in range(len(nums) - 1, - 1, -1):
+            out[i] = out[i] * p
+            p = p * nums[i]
         
-        else:
-            for j in range(0, len(nums)):
-                res[j] = sum // newHash[j]
-            
-        
-        return res
+        return out
