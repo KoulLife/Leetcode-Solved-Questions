88cba64a793721370d39c9c47f4dac249df40099
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2023-07-06 13:59:36 +0900

    Time: 89 ms (79.43%), Space: 20 MB (48.63%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -11,9 +11,3 @@ class NumArray:
         rightSum = self.prefix[right]
         leftSum = self.prefix[left - 1] if left > 0 else 0
         return rightSum - leftSum
-        
-
-
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# param_1 = obj.sumRange(left,right)
